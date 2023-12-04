bb29880d31636df14a3a63be271f1a7d1ad82264
Emir Risyad <emirrsyd@gmail.com>
2023-12-04 01:09:06 +0100

    landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,13 +16,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      className='h-full'>
       <body
         className={cn(
           'relative h-full font-sans antialiased',
           inter.className
         )}>
-        {children}
+        <main className='relative flex flex-col min-h-screen'>
+          <div className='flex-grow flex-1'>{children}</div>
+        </main>
       </body>
     </html>
   );
